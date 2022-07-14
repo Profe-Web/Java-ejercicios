6022d1a5835962eb3744d9567a476e8ab4741784
Profe-Web <106133932+Profe-Web@users.noreply.github.com>
2022-07-14 11:29:48 +0200

    Add files via upload

---
+++ array3/src/main/java/com/mycompany/array3/Array3.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.array3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Profe
+ */
+
+public class Array3 {
+
+    public static void main(String[] args) {
+        
+        for(int i=0; i<=10; i+=2){
+            System.out.println(i);
+        }
+        
+    }
+}
---
+++ array3/src/main/java/com/mycompany/array3/test3.java
@@ -0,0 +1,15 @@
+
+
+
+package com.mycompany.array3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Profe
+ */
+
+public class test3 {
+    
+}
