951b0f8db1e2a454236138399ec8d80be408495f
Profe-Web <106133932+Profe-Web@users.noreply.github.com>
2022-09-30 11:38:59 +0200

    Add files via upload
    API Spring Boot solo con listar y guardar. Del video: https://www.youtube.com/watch?v=vTu2HQrXtyw&t=1s

---
+++ Spring Boot API (Minimo)/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+// código completo en:
+// https://github.com/adamofig/apiRestSpringBootUsuarios
